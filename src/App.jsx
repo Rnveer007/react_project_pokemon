ac95ce1d8589341eb96336876c7f91425d6d8bb8
Rnveer007 <rvs523474@gmail.com>
2025-01-20 17:37:33 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -39,33 +39,33 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Poke - World</h1>
-        <div>
-          <select name="" id="">
+      <div className="bg-black h-full py-8">
+        <h1 className="text-yellow-800 text-6xl text-center">Poke - World</h1>
+        <div className="flex justify-center my-6">
+          <select name="" id="" className="w-30 py-2 capitalize">
             <option value="" disabled>Filter by type</option>
             <option value="all">All Types</option>
             {
               allTypes.map((object, index) => {
                 return (
-                  <option className="capitalize" key={index} value={object.name}>{object.name}</option>
+                  <option key={index} value={object.name} className="capitalize">{object.name}</option>
                 )
               })
             }</select>
-          <input type="text" placeholder="Search for Anything" />
+          <input type="text" placeholder="Search for Anything" className="w-80 py-2 pl-4 capi" />
         </div>
       </div>
 
-      <div>
-        <h1>Pokemon list</h1>
-        <div>
+      <div className="bg-gray-900	">
+        <h1 className="text-white text-5xl text-center py-8">Pokemon-List</h1>
+        <div className="flex flex-wrap gap-4 items-center justify-evenly px-10">
           {
             displayPokemons.map((object) => {
               return (
-                <div key={object.id}>
-                  <img src={object.sprites.other.dream_world.front_default} alt="" />
-                  <h1>{object.name}</h1>
-                  <p> {object.types.map(obj => obj.type.name).toString()}</p>
+                <div key={object.id} className="w-[22%] text-center mb-6">
+                  <img src={object.sprites.other.dream_world.front_default} className="w-full h-56" />
+                  <h1 className="text-white text-2xl font-bold capitalize my-2">{object.name}</h1>
+                  <p className="text-white font-bold my-2 capitalize"> {object.types.map(obj => obj.type.name).toString()}</p>
                 </div>
               )
 
